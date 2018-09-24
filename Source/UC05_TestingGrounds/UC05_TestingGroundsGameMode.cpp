847632b22b4becc515ef30b6c5167ba8c73743a5
Gregory Scott Hanna <eagle_knight_22@hotmail.com>
2018-09-24 12:39:05 -0400

    Moved several files to Dynamic sub-folder (Not to be tracked by LFS)

--- Source/UC05_TestingGrounds/UC05_TestingGroundsGameMode.cpp
+++ Source/UC05_TestingGrounds/UC05_TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AUC05_TestingGroundsGameMode::AUC05_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
